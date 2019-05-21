8b5453fca919b4328fc0c06e89635cd999fc47d2
Adrian Syahputra Alfinda <niko2nii69@gmail.com>
2019-05-21 23:22:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tubesdesu/FragmentTampil.java
@@ -0,0 +1,52 @@
+/*
+        Tanggal Pengerjaan : 18-05-2019
+        Nim : 10116172
+        Nama : Adrian Syahputra Alfinda
+        Kelas : IF-4 (AKB-4) >
+
+*/
+
+
+package com.example.tubesdesu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.w3c.dom.ls.LSException;
+
+public class FragmentTampil extends Fragment {
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view =  inflater.inflate(R.layout.activity_fragment_tampil, container, false);
+
+        Button buttonPut = (Button) view.findViewById(R.id.buttonInput);
+        Button buttonLi = (Button) view.findViewById(R.id.buttonLihat);
+        buttonPut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent inta = new Intent(getActivity(), TambahTeman.class);
+                startActivity(inta);
+            }
+            });
+
+        buttonLi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intas = new Intent(getActivity(), ListTeman.class);
+                startActivity(intas);
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/tubesdesu/LihatPager.java
@@ -0,0 +1,43 @@
+/*
+        Tanggal Pengerjaan : 18-05-2019
+        Nim : 10116172
+        Nama : Adrian Syahputra Alfinda
+        Kelas : IF-4 (AKB-4) >
+
+*/
+
+
+package com.example.tubesdesu;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+
+public class LihatPager extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_pager);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPage(this));
+
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textView26:
+                startActivity(new Intent(this, BottomActivity.class));
+                break;
+        }
+    }
+}
